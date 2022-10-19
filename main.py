6a47abc711770cf1fe705624053f564c05af622f
Mark Valentine Aikins <valentineaikins@icloud.com>
2022-10-19 18:04:52 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import math
+pi = math.pi
+print(pi)
+print(round(pi))
+print(math.ceil(pi))
+print(math.floor(pi))
+print(math.sqrt(pi))
+x, y, z, w = 10, 3, 20, 5
+print(abs(pi))
+print(pow(pi, 3))
+print(max(x,y,z,w))
+print(min(x,y,z,w))
+print(math.factorial(x)) #print  factorial each element
